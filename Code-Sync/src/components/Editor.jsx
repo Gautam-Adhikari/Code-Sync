f94a0ab96b07fe3c03c5c0276d25b534ef654b07
Gautam Adhikari <gautamadhikari831@gmail.com>
2024-08-11 00:32:38 +0530

    codemirror implent

--- Code-Sync/src/components/Editor.jsx
+++ Code-Sync/src/components/Editor.jsx
@@ -5,24 +5,44 @@ import 'codemirror/theme/dracula.css';
 import 'codemirror/addon/edit/closetag';
 import 'codemirror/addon/edit/closebrackets';
 import 'codemirror/lib/codemirror.css'
+import { useRef } from 'react';
 
 
 const Editor = () => {
-    useEffect(()=>{
-        async function init(){
-            Codemirror.fromTextArea(
-                document.getElementById("realtimeEditor"), 
-                {
-                mode: { name: "javascript", json: true },
-                theme: "dracula",
-                autoCloseTags: true,
-                autoCloseBrackets: true,
-                lineNumbers: true,
-                }
-            );
-            }
-        init();
-    },[])
+   
+    const editorRef = useRef(null);
+
+    useEffect(() => {
+      const init = async () => {
+        editorRef.current = Codemirror.fromTextArea(
+          document.getElementById("realtimeEditor"),
+          {
+            mode: { name: "javascript", json: true },
+            theme: "dracula",
+            autoCloseTags: true,
+            autoCloseBrackets: true,
+            lineNumbers: true,
+          }
+        );
+  
+        editorRef.current.on("change", (instance, changes) => {
+          const { origin } = changes;
+          const code = instance.getValue();
+          onCodeChange(code);
+  
+          if (origin !== "setValue") {
+            socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+              roomId,
+              code,
+            });
+          }
+        });
+      };
+  
+      init();
+    }, []);
+  
+    
     return <textarea id="realtimeEditor"></textarea>;
 }
 
