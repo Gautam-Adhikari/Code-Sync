0dcd70b521eacea57e9f8967ca70457626d72766
Gautam Adhikari <gautamadhikari831@gmail.com>
2024-08-21 14:55:24 +0530

    production build

--- Code-Sync/server.js
+++ Code-Sync/server.js
@@ -8,10 +8,13 @@ const ACTIONS = require('./src/Actions.cjs');
 const server = http.createServer(app);
 const io = new Server(server);
 
-// app.use(express.static('build'));
-// app.use((req, res, next) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// })
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// For all other routes, serve the index.html file from the 'dist' directory
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 const userSocketMap = {};
 
